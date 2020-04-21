e7687341527a92e001deb46dd76b574ea12f3ec5
Pereirarthur <pereirarthur01@gmail.com>
2020-04-21 20:40:39 -0300

    Projeto Criado

---
+++ src/main/java/com/instituto/projetodocurso/ProjetodocursoApplication.java
@@ -0,0 +1,13 @@
+package com.instituto.projetodocurso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetodocursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetodocursoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/instituto/projetodocurso/ProjetodocursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.instituto.projetodocurso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetodocursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
