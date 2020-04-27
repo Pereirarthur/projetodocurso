d8cb4cca1ae359d5c4d426278f3df733c950dd6a
Pereirarthur <pereirarthur01@gmail.com>
2020-04-27 18:22:03 -0300

    ItemPedido e ItemPedidoPK

--- src/main/java/com/instituto/projetodocurso/ProjetodocursoApplication.java
+++ src/main/java/com/instituto/projetodocurso/ProjetodocursoApplication.java
@@ -13,6 +13,7 @@ import com.instituto.projetodocurso.domain.Cidade;
 import com.instituto.projetodocurso.domain.Cliente;
 import com.instituto.projetodocurso.domain.Endereco;
 import com.instituto.projetodocurso.domain.Estado;
+import com.instituto.projetodocurso.domain.ItemPedido;
 import com.instituto.projetodocurso.domain.Pagamento;
 import com.instituto.projetodocurso.domain.PagamentoComBoleto;
 import com.instituto.projetodocurso.domain.PagamentoComCartao;
@@ -25,6 +26,7 @@ import com.instituto.projetodocurso.repositories.CidadeRepository;
 import com.instituto.projetodocurso.repositories.ClienteRepository;
 import com.instituto.projetodocurso.repositories.EnderecoRepository;
 import com.instituto.projetodocurso.repositories.EstadoRepository;
+import com.instituto.projetodocurso.repositories.ItemPedidoRepository;
 import com.instituto.projetodocurso.repositories.PagamentoRepository;
 import com.instituto.projetodocurso.repositories.PedidoRepository;
 import com.instituto.projetodocurso.repositories.ProdutoRepository;
@@ -48,7 +50,8 @@ public class ProjetodocursoApplication implements CommandLineRunner{
 	private PedidoRepository pedidoRepositorio;
 	@Autowired
 	private PagamentoRepository pagamentoRepositorio;
-	
+	@Autowired
+	private ItemPedidoRepository itemPedidoReposotorio;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetodocursoApplication.class, args);
@@ -110,6 +113,18 @@ public class ProjetodocursoApplication implements CommandLineRunner{
 		cli1.getPedidos().addAll(Arrays.asList(ped1,ped2));
 		
 		
+		
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0, 1, 2000);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0, 2, 80);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100, 1, 800);
+		
+		ped1.getItens().addAll(Arrays.asList(ip1,ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+		
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+		
 		estadoRepositorio.saveAll(Arrays.asList(est1,est2));
 		cidadeRepositorio.saveAll(Arrays.asList(c1,c2,c3));
 		clienteRepositorio.saveAll(Arrays.asList(cli1));
@@ -118,6 +133,7 @@ public class ProjetodocursoApplication implements CommandLineRunner{
 		produtoRepositorio.saveAll(Arrays.asList(p1,p2,p3));
 		pedidoRepositorio.saveAll(Arrays.asList(ped1,ped2));
 		pagamentoRepositorio.saveAll(Arrays.asList(pagto1,pagto2));
+		itemPedidoReposotorio.saveAll(Arrays.asList(ip1,ip2,ip3));
 	}
 
 }
